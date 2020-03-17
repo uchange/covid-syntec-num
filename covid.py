3992ab0ea39bd503473d64af3cf81836eafcaefa
Marc <marc@debureaux.fr>
2020-03-17 22:22:25 +0100

    Adding counts on pages and categories

--- covid.py
+++ covid.py
@@ -9,12 +9,13 @@ from flask_caching import Cache
 
 URL = os.environ.get('URL')
 TOKEN = os.environ.get('TOKEN')
-WS_FAMILLE, WS_TRAVAIL, WS_SANTE, WS_SANTE_PRO = (
-    os.environ.get('WS_FAMILLE'),
-    os.environ.get('WS_TRAVAIL'),
-    os.environ.get('WS_SANTE'),
-    os.environ.get('WS_SANTE_PRO'),
-)
+HEADERS = {'Authorization': f'Token {TOKEN}'}
+WORKSPACES = {
+    'famille': os.environ.get('WS_FAMILLE'),
+    'travail': os.environ.get('WS_TRAVAIL'),
+    'sante': os.environ.get('WS_SANTE'),
+    'sante_pro': os.environ.get('WS_SANTE_PRO'),
+}
 
 config = {
     'CACHE_TYPE': os.environ.get('CACHE_TYPE', 'filesystem'),
@@ -31,11 +32,15 @@ logger.setLevel(os.environ.get('LOGLEVEL', 'INFO'))
 
 
 def get_startups(workspace, category=None):
+    """
+    Get startups from Motherbase with activities, entity types, linkedin and twitter accounts
+    """
+    if not workspace:
+        return {}
     try:
+        startups = {}
         with requests.Session() as session:
-            session.headers.update({
-                'Authorization': f'Token {TOKEN}',
-            })
+            session.headers.update(HEADERS)
             # Startups
             results = session.get(URL + '/api/startup/', params=dict(
                 fields=','.join((
@@ -55,7 +60,6 @@ def get_startups(workspace, category=None):
                 **(dict(links__extra_data__category=category) if category else {})
             ))
             logger.debug(f"[{results.elapsed}] {results.url}")
-            startups = {}
             for result in results.json():
                 startups[result['id']] = result
                 if not result['logo']:
@@ -119,77 +123,120 @@ def get_startups(workspace, category=None):
                 startup = startups[result['company_id']]
                 element = startup.setdefault('twitter', [])
                 element.append(result)
-            # Return results
-            return startups.values()
+        # Return results
+        return startups.values()
     except:  # noqa
         traceback.print_exc()
     return None
 
 
 def get_categories(workspace):
+    """
+    Get categories of a workspace
+    """
+    if not workspace:
+        return []
     try:
         with requests.Session() as session:
-            session.headers.update({
-                'Authorization': f'Token {TOKEN}',
-            })
+            session.headers.update(HEADERS)
             results = session.get(URL + '/api/front/attribute/', params=dict(
                 name='category',
                 workspace_id=workspace,
                 all=1
-            )).json()
-            for result in results:
+            ))
+            logger.debug(f"[{results.elapsed}] {results.url}")
+            for result in results.json():
                 return sorted(result['enum'], key=lambda e: e['value'])
     except:  # noqa
         traceback.print_exc()
     return []
 
 
+def get_counts(workspace=None):
+    """
+    Get counts of startups for all workspaces and eventually a targetted one
+    """
+    try:
+        counts, subcounts = {}, {}
+        with requests.Session() as session:
+            session.headers.update(HEADERS)
+            # Counts by workspace
+            results = session.get(URL + '/api/front/link', params=dict(
+                workspace__in=','.join(WORKSPACES.values()),
+                company__startup__isnull=False,
+                group_by='workspace_id',
+                count='id',
+                all=1,
+            ))
+            logger.debug(f"[{results.elapsed}] {results.url}")
+            pages = {wid: page for page, wid in WORKSPACES.items()}
+            for result in results.json():
+                page = pages.get(str(result['workspace_id']))
+                counts[page] = result['id_count']
+            # Counts by category in workspace
+            if workspace:
+                results = session.get(URL + '/api/front/link', params=dict(
+                    workspace=workspace,
+                    company__startup__isnull=False,
+                    group_by='extra_data__category',
+                    count='id',
+                    all=1,
+                ))
+                logger.debug(f"[{results.elapsed}] {results.url}")
+                for result in results.json():
+                    subpage = result['extra_data__category']
+                    subcounts[subpage] = result['id_count']
+        # Return results
+        return counts, subcounts
+    except:
+        raise
+        return {}, {}
+
+
+def get_page(page, category=None):
+    """
+    Shortcut to return a page of startups given a page name and a category
+    """
+    workspace = WORKSPACES.get(page)
+    startups = get_startups(workspace, category)
+    categories = get_categories(workspace)
+    counts, subcounts = get_counts(workspace)
+    return render_template(
+        'main.html', page=page, subpage=category,
+        counts=counts, subcounts=subcounts,
+        startups=startups, categories=categories)
+
 
 @app.route('/')
 @cache.cached()
 def about():
-    return render_template('about.html', page='about')
+    counts, subcounts = get_counts()
+    return render_template('about.html', page='about', counts=counts)
 
 
 @app.route('/famille/')
 @app.route('/famille/<category>/')
 @cache.cached()
 def famille(category=None):
-    startups = get_startups(WS_FAMILLE, category)
-    categories = get_categories(WS_FAMILLE)
-    return render_template(
-        'main.html', page='famille', subpage=category,
-        startups=startups, categories=categories)
+    return get_page('famille', category)
 
 
 @app.route('/travail/')
 @app.route('/travail/<category>/')
 @cache.cached()
 def travail(category=None):
-    startups = get_startups(WS_TRAVAIL, category)
-    categories = get_categories(WS_TRAVAIL)
-    return render_template(
-        'main.html', page='travail', subpage=category,
-        startups=startups, categories=categories)
+    return get_page('travail', category)
 
 
 @app.route('/sante/')
 @app.route('/sante/<category>/')
 @cache.cached()
 def sante(category=None):
-    startups = get_startups(WS_SANTE, category)
-    categories = get_categories(WS_SANTE)
-    return render_template(
-        'main.html', page='sante', subpage=category,
-        startups=startups, categories=categories)
+    return get_page('sante', category)
 
 
 @app.route('/sante-pro/')
 @app.route('/sante-pro/<category>/')
 @cache.cached()
 def sante_pro(category=None):
-    startups = get_startups(WS_SANTE_PRO, category)
-    categories = get_categories(WS_SANTE_PRO)
-    return render_template(
-        'main.html', page='sante_pro', subpage=category,
-        startups=startups, categories=categories)
+    return get_page('sante_pro', category)
